657c26e504988f9fb1e846c2ca8074c207a35a41
goutkannan <gout.kannan@gmail.com>
2017-07-28 15:55:18 -0500

    Algo:Implementation Encryption

---
+++ DS/encryption.py
@@ -0,0 +1,22 @@
+import sys
+import math
+s="haveaniceday"
+s = s.replace(" ","")
+
+n = len(s)
+
+floor = math.floor(math.sqrt(n))
+ceil = math.ceil(math.sqrt(n))
+
+row = floor
+column = ceil 
+
+while(row*column < n):
+  if(floor<=row<=column<=ceil):
+    row+=1
+    
+strlen=0
+for i in range(0,row+1):
+    if(strlen<n):
+      print(s[i::column],end=" ")
+      strlen+=len(s[i::column])
